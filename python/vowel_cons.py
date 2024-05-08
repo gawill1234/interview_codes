42a6e4a7cd77cb0a34c87f3a8491c207b6908cf5
Gary Williams <gawilliams@us.ibm.com>
2024-05-08 14:05:39 -0400

    Add python code to count vowels and consonants.

---
+++ python/vowel_cons.py
@@ -0,0 +1,38 @@
+#
+#  Count vowels and consonants in a string.
+#  First make sure what you are looking at
+#  is a letter, not some other character.
+#  Then see if it is a vowel or not.
+#
+import sys, string
+
+vowels = "aeiou"
+alphabet = string.ascii_lowercase
+ 
+def vowel(mystring):
+
+   vowel_count = cons_count = 0
+
+   for letter in mystring:
+      if (letter in alphabet):
+         if (letter in vowels):
+            vowel_count += 1
+         else:
+            cons_count += 1
+
+
+   return vowel_count, cons_count
+ 
+if __name__ == "__main__":
+
+   mystring = 'abcdefghijklmnopqrstuvwxyz'
+   v, c = vowel(mystring.lower())
+   print("vowels: ", v, "  consonants: ", c)
+   mystring = 'ABC! DEF^ G..,, HIJKLMNOPQRSTUVWXYZ'
+   v, c = vowel(mystring.lower())
+   print("vowels: ", v, "  consonants: ", c)
+   mystring = 'This is a real sentence.  With punctuation and everything!'
+   v, c = vowel(mystring.lower())
+   print("vowels: ", v, "  consonants: ", c)
+   sys.exit(0)
+
