308e4ab00c55e908bf6540b3fadcef20cb1ce332
Gary Williams <gawilliams@us.ibm.com>
2024-05-09 12:41:40 -0400

    caesar with a longer alphabet

---
+++ python/caesar2.py
@@ -0,0 +1,47 @@
+import string
+
+#
+#  Caesar cypher using python3 translate
+#  and maketrans
+#
+
+alphabet = string.ascii_uppercase + string.ascii_lowercase + " "
+
+def caesar(msg, key):
+    return msg.translate(
+        str.maketrans(alphabet, alphabet[key:] + alphabet[:key])
+    )
+
+def decryptit(key):
+    return(len(alphabet) - key)
+
+def runnit(mystring, key):
+   
+   xx = caesar(mystring, key)
+   print("key:", key, "  ", xx)
+   new_key = decryptit(key)
+   yy = caesar(xx, new_key)
+   print("key:", new_key, "  ", yy)
+
+   return
+
+
+if __name__ == "__main__":
+
+   key = 19
+   runnit("ABCDEFGHIJKLMNOPQRSTUVWXYZ", key)
+
+   print("##################")
+
+   key = 13
+   runnit("SQUIRREL FART ON A CHIPMUNK", key)
+
+   print("##################")
+
+   key = 22 
+   runnit("THE NINTH LEVEL OF HELL IS JOB HUNTING", key)
+
+   print("##################")
+
+   key = 38 
+   runnit("A string with a MIX of upper and lower cases", key)
