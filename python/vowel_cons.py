92ffd20b0c104f54fb96cd1ba0224082c37206c1
Gary Williams <gawilliams@us.ibm.com>
2024-05-09 11:56:48 -0400

    Move all of the work to the work function.

--- python/vowel_cons.py
+++ python/vowel_cons.py
@@ -13,7 +13,7 @@ def vowel(mystring):
 
    vowel_count = cons_count = 0
 
-   for letter in mystring:
+   for letter in mystring.lower():
       if (letter in alphabet):
          if (letter in vowels):
             vowel_count += 1
@@ -26,13 +26,16 @@ def vowel(mystring):
 if __name__ == "__main__":
 
    mystring = 'abcdefghijklmnopqrstuvwxyz'
-   v, c = vowel(mystring.lower())
+   v, c = vowel(mystring)
+   print("String: ", mystring)
    print("vowels: ", v, "  consonants: ", c)
    mystring = 'ABC! DEF^ G..,, HIJKLMNOPQRSTUVWXYZ'
-   v, c = vowel(mystring.lower())
+   v, c = vowel(mystring)
+   print("String: ", mystring)
    print("vowels: ", v, "  consonants: ", c)
    mystring = 'This is a real sentence.  With punctuation and everything!'
-   v, c = vowel(mystring.lower())
+   v, c = vowel(mystring)
+   print("String: ", mystring)
    print("vowels: ", v, "  consonants: ", c)
    sys.exit(0)
 
