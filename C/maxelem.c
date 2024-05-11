fe00f424dcddea8605aba7088820f8e92d3784a8
Gary Williams <gawilliams@us.ibm.com>
2024-05-10 23:44:56 -0400

    Add a simple max element program.

---
+++ C/maxelem.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxelem(int *elemarr, int arrsz) {
+int i, curmax;
+
+   /*
+    *  Sorry, there is nothing fancy or tricky
+    *  about finding the largest element in an
+    *  array.  You have to look at all of the 
+    *  elements.  This uses an integer array.
+    */
+   curmax = elemarr[0];
+   for (i = 0; i < arrsz; i++) {
+      if (elemarr[i] > curmax) {
+         curmax = elemarr[i];
+      }
+   }
+
+   return(curmax);
+}
+
+int main() {
+int elemarr[] = {401, 1, 99, 3, 5, 8, 462, 102, 4, 199, 301, 14, 87, 71, 5, 9, 200, 151, 88, 400};
+int arrmax;
+
+   /*
+    *  As long as the array is in scope, you can get its
+    *  size/length as below.  If you pass the array to 
+    *  another function, this method is no longer possible.
+    *  you'll have to pass the length of the array along 
+    *  with the array.
+    *
+    *  size/length here mean "number of elements"
+    */
+   arrmax = maxelem(elemarr, sizeof(elemarr) / sizeof(elemarr[0]));
+
+   printf("Max element: %d\n", arrmax);
+   fflush(stdout);
+}
