8cfd936155baa39ba3a276f8ac9d78889e82f5f1
Gary Williams <gawilliams@us.ibm.com>
2024-05-16 13:30:51 -0400

    Python sum a list

---
+++ python/sum_list.py
@@ -0,0 +1,53 @@
+import sys
+
+#
+#   This way you can keep a running
+#   total, if you want, in another list
+#
+def adder1(mylist):
+
+   print("ADDER1(for) values")
+   total = 0
+   newlist = []
+
+   for item in mylist:
+      total += item
+      newlist.append(total)
+
+   #
+   #   Print original list
+   #
+   print(mylist)
+   #
+   #   Print new list with
+   #   running total
+   #
+   print(newlist)
+
+   return
+
+#
+#   sum returns a total, not a list
+#
+def adder2(mylist):
+
+   print("ADDER2(sum) values")
+   total = sum(mylist)
+
+   #
+   #   Print original list
+   #
+   print(mylist)
+   #
+   #   Print total
+   #
+   print(total)
+
+   return
+
+if __name__ == "__main__":
+
+   mylist = [1, 2, 3, 4, 5, 6]
+
+   adder1(mylist)
+   adder2(mylist)
