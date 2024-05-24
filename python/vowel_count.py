5c05ebf3e71dde42602c6ceab3a6c79b41376316
Gary Williams <gawilliams@us.ibm.com>
2024-05-24 14:02:16 -0400

    Add a second method

--- python/vowel_count.py
+++ python/vowel_count.py
@@ -2,10 +2,42 @@
 #  Count individual vowels in a string.
 #
 import sys, string
+
+#
+#   Two ways.  map() and the switch
+#   like if/else.  If you use python
+#   3.10 or greater, you can use an
+#   actual switch statement.  Not
+#   assuming that here so that method
+#   is not included.
+#
  
 def vowel(mystring):
 
    a, e, i, o, u = map(mystring.lower().count, "aeiou")
+
+   return a, e, i, o, u
+
+def vowel2(mystring):
+   vowels = "aeiou"
+
+   a = e = i = o = u = 0
+   for item in mystring.lower():
+      if (item in vowels):
+         if (item == 'a'):
+            a += 1
+         elif (item == 'e'):
+            e += 1
+         elif (item == 'i'):
+            i += 1
+         elif (item == 'o'):
+            o += 1
+         else:
+            #
+            #   We know it is a vowel and
+            #   only 'u' is left
+            #
+            u += 1
 
    return a, e, i, o, u
 
@@ -14,7 +46,9 @@ def runnit(mystring):
    print("#####")
    a, e, i, o, u = vowel(mystring)
    print("String: ", mystring)
-   print("A: ", a, ", E: ", e, ", I: ", i, ", O: ", o, ", U: ", u)
+   print("(map result    ) A: ", a, ", E: ", e, ", I: ", i, ", O: ", o, ", U: ", u)
+   a, e, i, o, u = vowel2(mystring)
+   print("(if/else result) A: ", a, ", E: ", e, ", I: ", i, ", O: ", o, ", U: ", u)
 
    return
  
