c595989f168a31b20fb1d9fe243f45c87b7d3523
Gary Williams <gawilliams@us.ibm.com>
2024-06-06 22:17:25 -0400

    add circumference function

--- C/circle_area.c
+++ C/circle_area.c
@@ -22,26 +22,41 @@ float areaofcircle2(float r) {
    return(PI * pow(r, 2));
 }
 
+/*
+ *   area using radius squared
+ *   by using pow() function.
+ *   Only reason math.h is included.
+ */
+float circumferenceofcircle(float r) {
+
+   return(2 * PI * r);
+}
+
 int main() {
 float a;
 
    a = 3.0;
-   printf("(mult) Circle radius: %f, Circle area: %f\n", a, areaofcircle(a));
-   printf("(pow)  Circle radius: %f, Circle area: %f\n", a, areaofcircle2(a));
+   printf("(mult) Circle radius: %f, Circle area  : %f\n", a, areaofcircle(a));
+   printf("(pow)  Circle radius: %f, Circle area  : %f\n", a, areaofcircle2(a));
+   printf("Circle circumference: %f, circumference: %f\n", a, circumferenceofcircle(a));
    printf("\n");
    a = 8.25;
-   printf("(mult) Circle radius: %f, Circle area: %f\n", a, areaofcircle(a));
-   printf("(pow)  Circle radius: %f, Circle area: %f\n", a, areaofcircle2(a));
+   printf("(mult) Circle radius: %f, Circle area  : %f\n", a, areaofcircle(a));
+   printf("(pow)  Circle radius: %f, Circle area  : %f\n", a, areaofcircle2(a));
+   printf("Circle circumference: %f, circumference: %f\n", a, circumferenceofcircle(a));
    printf("\n");
    a = 3.1415926;
-   printf("(mult) Circle radius: %2.7f, Circle area: %f\n", a, areaofcircle(a));
-   printf("(pow)  Circle radius: %2.7f, Circle area: %f\n", a, areaofcircle2(a));
+   printf("(mult) Circle radius: %f, Circle area  : %f\n", a, areaofcircle(a));
+   printf("(pow)  Circle radius: %f, Circle area  : %f\n", a, areaofcircle2(a));
+   printf("Circle circumference: %f, circumference: %f\n", a, circumferenceofcircle(a));
    printf("\n");
    a = 2.1;
-   printf("(mult) Circle radius: %f, Circle area: %f\n", a, areaofcircle(a));
-   printf("(pow)  Circle radius: %f, Circle area: %f\n", a, areaofcircle2(a));
+   printf("(mult) Circle radius: %f, Circle area  : %f\n", a, areaofcircle(a));
+   printf("(pow)  Circle radius: %f, Circle area  : %f\n", a, areaofcircle2(a));
+   printf("Circle circumference: %f, circumference: %f\n", a, circumferenceofcircle(a));
    printf("\n");
    a = 1.0;
-   printf("(mult) Circle radius: %f, Circle area: %f\n", a, areaofcircle(a));
-   printf("(pow)  Circle radius: %f, Circle area: %f\n", a, areaofcircle2(a));
+   printf("(mult) Circle radius: %f, Circle area  : %f\n", a, areaofcircle(a));
+   printf("(pow)  Circle radius: %f, Circle area  : %f\n", a, areaofcircle2(a));
+   printf("Circle circumference: %f, circumference: %f\n", a, circumferenceofcircle(a));
 }
