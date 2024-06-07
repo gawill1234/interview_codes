a456299f81c7a5a0b8595dde5d0221331e0a6d24
Gary Williams <gawilliams@us.ibm.com>
2024-06-06 23:36:40 -0400

    Add third way, Make output more sensible.

--- C/prime/prime_comp.c
+++ C/prime/prime_comp.c
@@ -1,29 +1,3 @@
-/*
-   This program illustrates many of the questions which are used as
-   interview questions at FORE(and other places) to help determine
-   competency in the C programming language.  The items which are
-   in this program are:
-
-      function pointers
-      check alignment of (void *)
-      num bits(many different ones)
-         mask and shift
-         value & (value - 1)
-         value % 2      (2 different)
-         table look up  (2 different, both good 0 to 255, not just 127)
-      number odd or even
-         value % 2
-         value & 01
-      string reverse(in place with one temp variable, 3 different)
-      is a machine big endian or little endian(how to determine)
-      sentence reverse(words interchanged, not letters within words)
-
-   Others which are not here yet:
-      detecting a cycle in a linked list
-      inserting an item in a linked list
-      remove an item from a linked list
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -35,6 +9,11 @@ typedef int boolean;
 #define FALSE 0
 #define TRUE 1
 
+/*
+ *   Time how long it takes to get
+ *   a series of prime numbers using
+ *   various methods (do-while, while, for).
+ */
 //
 //   Return the elapsed time based on end_time (et)
 //   minus start_time (st).
@@ -75,6 +54,38 @@ boolean get_prime3(int mynumber)
          return(FALSE);
       }
    }
+
+   return(TRUE);
+}
+
+boolean get_prime1(int mynumber)
+{
+int mood;
+
+   if (mynumber == 1)
+      return(FALSE);
+
+   /* 
+    *   Lets just accept that we know 2 and 3 are prime numbers
+    */
+
+   if (mynumber <= 3)
+      return(TRUE);
+
+   /*
+    *  Any even number greater than 2 is by definition not a
+    *  prime number so we will skip them.
+    */
+   if ((mynumber % 2) == 0)
+      return(FALSE);
+
+   mood = 1;
+   do {
+      mood += 2;
+      if ((mynumber % mood) == 0) {
+         return(FALSE);
+      }
+   } while ((mood * mood) <= mynumber);
 
    return(TRUE);
 }
@@ -120,6 +131,24 @@ int mood;
    return(TRUE);
 }
 
+int do_primes1(int max)
+{
+int count;
+int x, y;
+
+
+   x = 2;
+   count = 0;
+   while (count < max) {
+      if (get_prime1(x) == TRUE) {
+         count += 1;
+         // printf("Prime found(do_primes1):   %d,    count: %d\n", x, count);
+      }
+      x++;
+   }
+   return(0);
+};
+
 int do_primes2(int max)
 {
 int count;
@@ -131,7 +160,7 @@ int x, y;
    while (count < max) {
       if (get_prime2(x) == TRUE) {
          count += 1;
-         //printf("Prime found(efficient):   %d,    count: %d\n", x, count);
+         //printf("Prime found(do_primes2):   %d,    count: %d\n", x, count);
       }
       x++;
    }
@@ -149,7 +178,7 @@ int x, y;
    while (count < max) {
       if (get_prime3(x) == TRUE) {
          count += 1;
-         //printf("Prime found(efficient):   %d,    count: %d\n", x, count);
+         //printf("Prime found(do_primes3):   %d,    count: %d\n", x, count);
       }
       x++;
    }
@@ -159,23 +188,44 @@ int x, y;
 int main()
 {
 struct timeval st, et;
-float amin, bmin;
+float amin, bmin, cmin;
 
    for (int z = 0; z < 10; z++) {
+      /*
+       *   Get prime with while loop
+       */
       gettimeofday(&st, NULL);
       do_primes2(100000);
       gettimeofday(&et, NULL);
       amin = (elapsed(st, et) * 1000000);
-      printf("A Primes time:  %f microseconds\n", amin);
+      printf("get_prime2() Primes time:  %f microseconds\n", amin);
+
+      /*
+       *   Get prime with for loop
+       */
       gettimeofday(&st, NULL);
       do_primes3(100000);
       gettimeofday(&et, NULL);
       bmin = (elapsed(st, et) * 1000000);
-      printf("B Primes time:  %f microseconds\n", bmin);
-      if (amin < bmin) {
-         printf("AAAAA Prime faster\n");
+      printf("get_prime3() Primes time:  %f microseconds\n", bmin);
+
+      /*
+       *   Get prime with do-while loop
+       */
+      gettimeofday(&st, NULL);
+      do_primes1(100000);
+      gettimeofday(&et, NULL);
+      cmin = (elapsed(st, et) * 1000000);
+      printf("get_prime1() Primes time:  %f microseconds\n", cmin);
+
+      if ((amin < bmin) && (amin < cmin)) {
+         printf("get_prime2 Prime faster\n");
+      } else if ((bmin < amin) && (bmin < cmin)) {
+         printf("get_prime3 Prime faster\n");
+      } else if ((cmin < amin) && (cmin < bmin)) {
+         printf("get_prime1 Prime faster\n");
       } else {
-         printf("BBBBB Prime faster\n");
+         printf("Mixed bag\n");
       }
    }
 }
