41042d64e1266bd33c8a9e276f1db31cbf83728d
Gary Williams <gawilliams@us.ibm.com>
2024-07-03 12:48:11 -0400

    add M to main

--- CSharp/rotate.cs
+++ CSharp/rotate.cs
@@ -146,7 +146,7 @@ namespace Rotate {
          return;
       }
       
-      static void main() {
+      static void Main() {
       int[] myarray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
       int[] newarray;
       int arrsz, shift;
