ffed2cece8def726a04a70c013ea644c84c1e68c
Gary Williams <gawilliams@us.ibm.com>
2026-01-09 22:29:19 -0500

    Using qsort

---
+++ C/sortwithqsort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+int compare_chars(const void *a, const void *b) {
+    /*
+     *  Cast the void pointers to char pointers and dereference them
+     */
+    return (*(char *)a - *(char *)b);
+}
+
+
+void runnit(char *mystring) {
+int n;
+
+    printf("String before sorting: %s\n", mystring);
+    n = strlen(mystring);
+    qsort(mystring, n, sizeof(char), compare_chars);
+    printf("String after sorting:  %s\n", mystring);
+
+    return;
+
+}
+
+int main() {
+char mystring[64];
+
+   strcpy(mystring, "hello");
+   runnit(mystring);
+   strcpy(mystring, "massive flood");
+   runnit(mystring);
+   strcpy(mystring, "mississippi");
+   runnit(mystring);
+   strcpy(mystring, "smile");
+   runnit(mystring);
+   strcpy(mystring, "limes");
+   runnit(mystring);
+   strcpy(mystring, "slime");
+   runnit(mystring);
+   strcpy(mystring, "multi space with punctuation!");
+   runnit(mystring);
+   strcpy(mystring, "2468097135");
+   runnit(mystring);
+}
