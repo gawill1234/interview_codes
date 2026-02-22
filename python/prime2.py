0ff7dd796fc50a5fdb5fe6e726bce94f048cb99e
Gary Williams <gawilliams@us.ibm.com>
2026-02-21 21:55:20 -0500

    Add some comments

--- python/prime2.py
+++ python/prime2.py
@@ -3,13 +3,22 @@
 #   using "yield".
 #
 def getprimes(mymax):
-    i = 2
-    while i < mymax:
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
-            yield i
-        i += 1
+   i = 2
+   while (i < mymax):
+      #
+      #   It is difficult for me to get used to the
+      #   idea of an "else" on a for loop.  But, here
+      #   it is.
+      #   The "else" code ONLY executes if the for loop
+      #   never hits a break
+      #
+      for j in range(2, i):
+         if ((i % j) == 0):
+            break
+      else:
+          yield i
+      i += 1
 
-print(list(getprimes(100)))
+if __name__ == "__main__":
+
+   print(list(getprimes(100)))
